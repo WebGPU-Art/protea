533755d6cf22a0ce0e346a58ea781dead1bbaec5
tiye <jiyinyiyong@gmail.com>
2023-04-29 21:16:12 +0800

    init project with code from webgpu samples

---
+++ README.md
@@ -0,0 +1,5 @@
+### Protea
+
+> Compute boids with Triadica controls
+
+based on https://webgpu.github.io/webgpu-samples/samples/computeBoids .
---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+declare module "*.wgsl" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.wgsl?raw" {
+  const value: string;
+  export default value;
+}
